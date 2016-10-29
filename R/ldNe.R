119840baafcd6927e494589757d2cd6480668c2d
Eric Archer <eric.archer@noaa.gov>
2016-10-29 10:25:31 -0400

    added by.strata calculation and maf.threshold removal to ldNe

--- R/ldNe.R
+++ R/ldNe.R
@@ -5,7 +5,10 @@
 #'
 #' @param g a \linkS4class{gtypes} object.
 #' @param maf.threshold smallest minimum allele frequency permitted to include 
-#'   a locus in calculation.
+#'   a locus in calculation of Ne.
+#' @param by.strata apply the \code{maf.threshold} by strata. if \code{TRUE} 
+#'   then any locus that is below this threshold in any strata will be removed 
+#'   from the calculation of Ne.
 #' @param ci central confidence interval.
 #'
 #' @return a named numeric vector with: 
@@ -32,7 +35,7 @@
 #' @importFrom stats cor qchisq
 #' @export
 #' 
-ldNe <- function(g, maf.threshold = 0, ci = 0.95) {
+ldNe <- function(g, maf.threshold = 0, by.strata = FALSE, ci = 0.95) {
   if(ploidy(g) != 2) stop("'g' must have diploid data")
   
   # remove non-biallelic loci
@@ -46,88 +49,95 @@ ldNe <- function(g, maf.threshold = 0, ci = 0.95) {
   
   # remove loci below MAF threshold
   if(maf.threshold > 0) {
-    maf.g <- maf(g)
-    above.thresh <- which(maf.g >= maf.threshold)
+    maf.g <- maf(g, by.strata = by.strata)
+    above.thresh <- if(by.strata) {
+      which(apply(maf.g, 2, function(x) all(x >= maf.threshold)))
+    } else {
+      which(maf.g >= maf.threshold)
+    }
     g <- g[, above.thresh, ]
   }
   
-  # convert gtypes to coded data.frame
-  g.df <- as.data.frame(g, ids = FALSE, strata = FALSE)
-  df <- do.call(cbind, lapply(seq(1, ncol(g.df), by = 2), function(a1) {
-    df.i <- g.df[, c(a1, a1 + 1)]
-    alleles <- sort(unique(unlist(df.i)))
-    apply(df.i, 1, function(i) sum(i == alleles[1]))
-  }))
-  
-  # remove loci that are constant
-  is.constant <- sapply(1:ncol(df), function(i) var(df[, i]) == 0)
-  df <- df[, !is.constant]
-  
-  # Eqn 1.7
-  calcNe <- function(S, Rsq.drift) {
-    if(S > 29) {
-      root.term <- 1 / 9 - 2.76 * Rsq.drift
-      if(root.term < 0) root.term <- 0
-      (1 / 3 + sqrt(root.term)) / (2 * Rsq.drift)
-    } else {
-      root.term <- 0.308 ^ 2 - 2.08 * Rsq.drift
-      if(root.term < 0) root.term <- 0
-      (0.308 + sqrt(root.term)) / (2 * Rsq.drift)
-    }
-  }
-  
-  # calculate correlation r-squared (rsq) between pairs of loci
-  loc.pairs <- combn(ncol(df), 2)
-  num.cores <- if(.Platform$OS.type == "windows") 1 else detectCores() - 1
-  loc.comp.mat <- do.call(rbind, mclapply(1:ncol(loc.pairs), function(i) {
-    pair.df <- df[, loc.pairs[, i]]
-    pair.df <- pair.df[complete.cases(pair.df), ]
-    rsq <- cor(pair.df[, 1], pair.df[, 2], method = "pearson") ^ 2
-    S <- nrow(pair.df)
-    c(S = S, rsq = rsq)
-  }, mc.cores = num.cores ))
-  
-  S <- loc.comp.mat[, "S"]
-  # Eqn 1.1: expected r-squared
-  E.rsq <- ifelse(S > 29, (1 / S) + (3.19 / S ^ 2), 0.0018 + (0.907 / S) + (4.44 / S ^ 2))
-  # sample size corrected r-squared
-  rsq <- loc.comp.mat[, "rsq"] * ((S / (S - 1)) ^ 2)
-  # Eqn 1.4
-  w <- loc.comp.mat[, "S"] ^ 2
-  # Eqn 1.5
-  W <- sum(w)
-  mean.rsq <- sum(w * rsq) / W
-  # Eqn 1.6
-  Rsq.drift <- rsq - E.rsq
-  # Eqn 1.10: R-squared prime.0 for Ne.0 
-  Rsq.drift.0 <- sum(Rsq.drift * w) / W
-  # harmonic mean of S
-  S.harm.mean <- nrow(loc.comp.mat) / sum(1 / S)
-  # initial Ne.0
-  ne0 <- calcNe(S.harm.mean, Rsq.drift.0)
-  
-  # Eqn 1.11: R-squared prime weights
-  wt <- S ^ 2 / (S + 3 * ne0) ^ 2
-  # Eqn 1.12: weighted R-squared prime
-  Rsq.drift <- sum(wt * Rsq.drift) / sum(wt)
-  
-  # mean expected r-squared
-  mean.E.rsq <- sum(w * E.rsq) / W
-  
-  # calculate CI
-  N <- nrow(loc.comp.mat)
-  lci.p <- (1 - ci) / 2
-  uci.p <- 1 - lci.p
-  Rsq.drift.lci <- mean.rsq * N / qchisq(lci.p, N) - mean.E.rsq
-  Rsq.drift.uci <- mean.rsq * N / qchisq(uci.p, N) - mean.E.rsq
+  # calculate Ne by strata
+  t(sapply(strataSplit(g), function(st.g) {
+    # convert gtypes to coded data.frame
+    g.df <- as.data.frame(st.g, ids = FALSE, strata = FALSE)
+    df <- do.call(cbind, lapply(seq(1, ncol(g.df), by = 2), function(a1) {
+      df.i <- g.df[, c(a1, a1 + 1)]
+      alleles <- sort(unique(unlist(df.i)))
+      apply(df.i, 1, function(i) sum(i == alleles[1]))
+    }))
     
-  c(
-    S = S.harm.mean, 
-    num.comp = ncol(loc.pairs), 
-    mean.rsq = mean.rsq,
-    mean.E.rsq = mean.E.rsq,
-    Ne = calcNe(S.harm.mean, Rsq.drift),
-    param.lci = calcNe(S.harm.mean, Rsq.drift.lci),
-    param.uci = calcNe(S.harm.mean, Rsq.drift.uci)
-  )
+    # remove loci that are constant
+    is.constant <- sapply(1:ncol(df), function(i) var(df[, i]) == 0)
+    df <- df[, !is.constant]
+    
+    # Eqn 1.7
+    calcNe <- function(S, Rsq.drift) {
+      if(S > 29) {
+        root.term <- 1 / 9 - 2.76 * Rsq.drift
+        if(root.term < 0) root.term <- 0
+        (1 / 3 + sqrt(root.term)) / (2 * Rsq.drift)
+      } else {
+        root.term <- 0.308 ^ 2 - 2.08 * Rsq.drift
+        if(root.term < 0) root.term <- 0
+        (0.308 + sqrt(root.term)) / (2 * Rsq.drift)
+      }
+    }
+    
+    # calculate correlation r-squared (rsq) between pairs of loci
+    loc.pairs <- combn(ncol(df), 2)
+    num.cores <- if(.Platform$OS.type == "windows") 1 else detectCores() - 1
+    loc.comp.mat <- do.call(rbind, mclapply(1:ncol(loc.pairs), function(i) {
+      pair.df <- df[, loc.pairs[, i]]
+      pair.df <- pair.df[complete.cases(pair.df), ]
+      rsq <- cor(pair.df[, 1], pair.df[, 2], method = "pearson") ^ 2
+      S <- nrow(pair.df)
+      c(S = S, rsq = rsq)
+    }, mc.cores = num.cores ))
+    
+    S <- loc.comp.mat[, "S"]
+    # Eqn 1.1: expected r-squared
+    E.rsq <- ifelse(S > 29, (1 / S) + (3.19 / S ^ 2), 0.0018 + (0.907 / S) + (4.44 / S ^ 2))
+    # sample size corrected r-squared
+    rsq <- loc.comp.mat[, "rsq"] * ((S / (S - 1)) ^ 2)
+    # Eqn 1.4
+    w <- loc.comp.mat[, "S"] ^ 2
+    # Eqn 1.5
+    W <- sum(w)
+    mean.rsq <- sum(w * rsq) / W
+    # Eqn 1.6
+    Rsq.drift <- rsq - E.rsq
+    # Eqn 1.10: R-squared prime.0 for Ne.0 
+    Rsq.drift.0 <- sum(Rsq.drift * w) / W
+    # harmonic mean of S
+    S.harm.mean <- nrow(loc.comp.mat) / sum(1 / S)
+    # initial Ne.0
+    ne0 <- calcNe(S.harm.mean, Rsq.drift.0)
+    
+    # Eqn 1.11: R-squared prime weights
+    wt <- S ^ 2 / (S + 3 * ne0) ^ 2
+    # Eqn 1.12: weighted R-squared prime
+    Rsq.drift <- sum(wt * Rsq.drift) / sum(wt)
+    
+    # mean expected r-squared
+    mean.E.rsq <- sum(w * E.rsq) / W
+    
+    # calculate CI
+    N <- nrow(loc.comp.mat)
+    lci.p <- (1 - ci) / 2
+    uci.p <- 1 - lci.p
+    Rsq.drift.lci <- mean.rsq * N / qchisq(lci.p, N) - mean.E.rsq
+    Rsq.drift.uci <- mean.rsq * N / qchisq(uci.p, N) - mean.E.rsq
+    
+    c(
+      S = S.harm.mean, 
+      num.comp = N, 
+      mean.rsq = mean.rsq,
+      mean.E.rsq = mean.E.rsq,
+      Ne = calcNe(S.harm.mean, Rsq.drift),
+      param.lci = calcNe(S.harm.mean, Rsq.drift.lci),
+      param.uci = calcNe(S.harm.mean, Rsq.drift.uci)
+    )
+  }))
 }
