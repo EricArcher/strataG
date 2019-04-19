734582d1f0fc0c23cb2e2832dbcea2e967088c78
Eric Archer <eric.archer@noaa.gov>
2019-04-19 11:16:44 -0700

    add landscape2gtypes function

---
+++ R/landscape2gtypes.R
@@ -0,0 +1,18 @@
+#' @title Create gtypes from Rmetasim landscape
+#' @description Create a gtypes object from an Rmetasim landscape object.
+#' 
+#' @param Rland rmetasim landscape object
+#' 
+#' @author Eric Archer \email{eric.archer@@noaa.gov}
+#' 
+#' @export
+#' 
+landscape2gtypes <- function(Rland) {
+  pl <- rmetasim::landscape.ploidy(Rland)
+  strata <- Rland$individuals[, 1] %/% Rland$intparam$stages + 1
+  gen.data <- Rland$individuals[, -(1:rmetasim::landscape.democol())]
+  loc.names <- paste0("Locus", 1:length(pl))
+  colnames(gen.data) <- paste(rep(loc.names, each = pl[1]), 1:pl[1], sep = ".")
+  gen.data <- cbind(id = Rland$individuals[, 4], strata = strata, gen.data)
+  df2gtypes(gen.data, ploidy = pl[1])
+}
